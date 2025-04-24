16dd58ffb3d6c0560403dc9e6af0e31fb5e26ca7
MAURICIO NADAL <mauricio.141205@bento.local>
2025-04-24 20:26:50 -0300

    sla

--- sandro/round_robin.py
+++ sandro/round_robin.py
@@ -10,27 +10,26 @@ class EscalonamentoRoundRobin:
 
 
     def executar(self):
-        pass
-#         tempo_atual = 0
-#         while self.fila:
-#             processo = self.fila.pop(0)
-#             print(f"Tempo {tempo_atual}: Executando {processo['nome']}")
+        tempo_atual = 0
+        while self.fila:
+            processo = self.fila.pop(0)
+            print(f"Tempo {tempo_atual}: Executando {processo['nome']}")
 
-#             if processo["tempo_restante"] > self.quantum:
-#                 processo["tempo_restante"] -= self.quantum
-#                 tempo_atual += self.quantum
-#                 self.fila.append(processo)
-#                 print(f"{processo['nome']} não terminou, resta {processo['tempo_restante']} unidades de tempo.")
-#             else:
-#                 tempo_atual += processo["tempo_restante"]
-#                 print(f"{processo['nome']} finalizado.")
+            if processo["tempo_restante"] > self.quantum:
+                processo["tempo_restante"] -= self.quantum
+                tempo_atual += self.quantum
+                self.fila.append(processo)
+                print(f"{processo['nome']} não terminou, resta {processo['tempo_restante']} unidades de tempo.")
+            else:
+                tempo_atual += processo["tempo_restante"]
+                print(f"{processo['nome']} finalizado.")
 
-#         print(f"Todos os processos foram executados em {tempo_atual} unidades de tempo.")
+        print(f"Todos os processos foram executados em {tempo_atual} unidades de tempo.")
 
 
-# escalonador = EscalonamentoRoundRobin(quantum=3)
-# escalonador.adicionar_processo("P1", 10)
-# escalonador.adicionar_processo("P2", 4)
-# escalonador.adicionar_processo("P3", 7)
+escalonador = EscalonamentoRoundRobin(quantum=3)
+escalonador.adicionar_processo("P1", 10)
+escalonador.adicionar_processo("P2", 4)
+escalonador.adicionar_processo("P3", 7)
 
-# escalonador.executar()
+escalonador.executar()
