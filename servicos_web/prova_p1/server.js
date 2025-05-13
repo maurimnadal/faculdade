a5b8e1579cd8a5af71fbe13e8a9207c92f4be4a2
maurimnadal <maurim.nadal@gmail.com>
2025-05-13 15:23:30 -0300

    add video.txt

--- servicos_web/prova_p1/server.js
+++ servicos_web/prova_p1/server.js
@@ -3,6 +3,7 @@ const app = express();
 const jogosRoute = require("./routes/jogos");
 const jogadoresRoute = require("./routes/jogadores")
 const pontuacoesRoute = require("./routes/pontuacoes")
+
 app.use(express.json());
 app.use("/jogos", jogosRoute);
 app.use("/jogadores", jogadoresRoute)
