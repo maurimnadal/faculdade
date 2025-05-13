3928a48c1870585aa4289285a1af1f03c7b89ef7
MAURICIO NADAL <mauricio.141205@bento.local>
2025-05-13 19:56:29 -0300

    correct prettier

--- servicos_web/loja-api/eslint.config.mjs
+++ servicos_web/loja-api/eslint.config.mjs
@@ -1,25 +1,30 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import prettierPlugin from 'eslint-plugin-prettier';
-import { defineConfig } from 'eslint/config';
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import prettier from 'eslint-config-prettier'
 export default defineConfig([
+    // Regras recomendadas do ESLint
     js.configs.recommended,
+    // Regras personalizadas + integração com Prettier
     {
-        files: ['**/*.js'],
+        files: ['**/*.{js,cjs}'],
         languageOptions: {
-            ecmaVersion: 2022,
-            sourceType: 'module',
-            globals: globals.node
+            sourceType: 'commonjs',
+            globals: globals.node,
+            ecmaVersion: 'latest',
         },
         plugins: {
-            prettier: prettierPlugin
+            prettier: eslintPluginPrettier,
         },
         rules: {
-            'prefer-const': 'error',
-            'no-console': 'warn',
-            'consistent-return': 'warn',
-            'prefer-template': 'warn',
-            'prettier/prettier': 'error'
-        }
-    }
-]);
+            'prefer-const': 'error', // Use const se possível
+            'no-console': 'warn', // Evita uso indiscriminado de console.log
+            'consistent-return': 'warn', // Retornos coerentes em funções
+            'prefer-template': 'warn', // Usa template strings ao invés de concatenação
+            'prettier/prettier': 'error', // Mostra erros de formatação do Prettier como erros do ESLint
+        },
+    },
+    // Desativa regras do ESLint que conflitam com o Prettier
+    prettier,
+])
