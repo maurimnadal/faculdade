ae13448bf38d8d1fccb885ad85e131509c743ded
maurimnadal <maurim.nadal@gmail.com>
2025-05-13 16:31:56 -0300

    projeto completo

---
+++ servicos_web/projeto-api/src/database/create_tables.sql
@@ -0,0 +1,9 @@
+-- Usa o banco correto
+USE projeto_api_db;
+-- Cria a tabela de usuários
+CREATE TABLE IF NOT EXISTS users (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ name VARCHAR(100) NOT NULL,
+ email VARCHAR(150) NOT NULL UNIQUE,
+ created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
