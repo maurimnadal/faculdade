abac9c3a261d75d524521bbd8f79daff2e46d078
MAURICIO NADAL <mauricio.141205@bento.local>
2025-09-10 20:03:01 -0300

    sla

---
+++ aplicacoes_corp/projeto-api/src/database/seed_data.sql
@@ -0,0 +1,6 @@
+-- Usa o banco
+USE projeto_api_db;
+-- Inserção de dados fictícios
+INSERT INTO users (name, email) VALUES
+('João da Silva', 'joao@example.com'),
+('Maria Oliveira', 'maria@example.com');
